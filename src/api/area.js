9482f91ddf24dd22ef4e833ed7944028e5cf6343
张一凡 <m13110683916@163.com>
2021-04-11 20:51:41 +0800

    实现分页

--- src/api/area.js
+++ src/api/area.js
@@ -6,3 +6,10 @@ export function getAllAreas() {
     method: 'get'
   })
 }
+
+export function fetchList(query) {
+  return request({
+    url: 'http://localhost:8888/admin/areas/list?pageSize=' + query.pageSize + '&pageNum=' + query.pageNum + '&orderBy=' + query.orderBy,
+    method: 'get'
+  })
+}
--- src/views/area/index.vue
+++ src/views/area/index.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="app-container">
     <h1 class="el-icon-location"> 地域管理</h1>
+<!--    <div class="filter-container">-->
+<!--      <el-input v-model="listQuery.title" placeholder="Title" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />-->
+<!--      <el-select v-model="listQuery.importance" placeholder="Imp" clearable style="width: 90px" class="filter-item">-->
+<!--        <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item" />-->
+<!--      </el-select>-->
+<!--      <el-select v-model="listQuery.type" placeholder="Type" clearable class="filter-item" style="width: 130px">-->
+<!--        <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.display_name+'('+item.key+')'" :value="item.key" />-->
+<!--      </el-select>-->
+<!--      <el-select v-model="listQuery.sort" style="width: 140px" class="filter-item" @change="handleFilter">-->
+<!--        <el-option v-for="item in sortOptions" :key="item.key" :label="item.label" :value="item.key" />-->
+<!--      </el-select>-->
+<!--      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">-->
+<!--        Search-->
+<!--      </el-button>-->
+<!--      <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="handleCreate">-->
+<!--        Add-->
+<!--      </el-button>-->
+<!--      <el-button v-waves :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download" @click="handleDownload">-->
+<!--        Export-->
+<!--      </el-button>-->
+<!--      <el-checkbox v-model="showReviewer" class="filter-item" style="margin-left:15px;" @change="tableKey=tableKey+1">-->
+<!--        reviewer-->
+<!--      </el-checkbox>-->
+<!--    </div>-->
     <el-table
       v-loading="listLoading"
       :data="list"
@@ -10,7 +34,7 @@
       stripe
       highlight-current-row
     >
-      <el-table-column align="center" label="ID" width="95">
+      <el-table-column align="center" prop="id" label="ID" width="95">
         <template slot-scope="scope">
           {{ scope.$index }}
         </template>
@@ -47,13 +71,31 @@
           <span>{{ scope.row.updateTime }}</span>
         </template>
       </el-table-column>
+      <el-table-column label="Actions" align="center" width="230" class-name="small-padding fixed-width">
+        <template slot-scope="{row,$index}">
+          <el-button type="primary" size="mini" @click="handleUpdate(row)">
+            编辑
+          </el-button>
+          <el-button v-if="row.status!='deleted'" size="mini" type="danger" @click="handleDelete(row,$index)">
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
     </el-table>
+
+    <pagination v-show="total>0" :total="total" :page.sync="listQuery.pageNum" :limit.sync="listQuery.pageSize" @pagination="getList" @current-change="pageChanged"/>
+
   </div>
 </template>
 
 <script>
-import { getAllAreas } from '@/api/area'
+import { getAllAreas, fetchList } from '@/api/area'
+import Pagination from '@/components/Pagination'
+import waves from '@/directive/waves' // waves directive
+
 export default {
+  components: { Pagination },
+  directives: { waves },
   filters: {
     statusFilter(status) {
       const statusMap = {
@@ -67,13 +109,30 @@ export default {
   data() {
     return {
       list: null,
-      listLoading: true
+      listLoading: true,
+      total: 0,
+      provinceSelects: null,
+      citySelects: null,
+      listQuery: {
+        pageSize: 10,
+        pageNum: 1,
+        orderBy: 'id ASC'
+      }
     }
   },
   created() {
-    this.fetchData()
+    this.getList()
   },
   methods: {
+    getList() {
+      this.listLoading = true
+      fetchList(this.listQuery).then(response => {
+        this.list = response.data.body.list
+        this.total = response.data.body.total
+        this.listLoading = false
+        console.log(this.listQuery.pageNum)
+      })
+    },
     fetchData() {
       this.listLoading = true
       getAllAreas().then(response => {
@@ -81,6 +140,39 @@ export default {
         this.list = response.data.body.list
         this.listLoading = false
       })
+    },
+    pageChanged() {
+      console.log(this.listQuery.pageNum)
+    },
+    // 执行过滤，重置为第一页
+    handleFilter() {
+      this.listQuery.page = 1
+      this.getList()
+    },
+    // 更改排序
+    sortChange(data) {
+      const { prop, order } = data
+      if (prop === 'id') {
+        this.sortByID(order)
+      }
+    },
+    sortByID(order) {
+      if (order === 'ascending') {
+        this.listQuery.sort = 'id ASC'
+      } else {
+        this.listQuery.sort = 'id DESC'
+      }
+      this.handleFilter()
+    },
+    createData() {
+    },
+    handleUpdate(row) {
+    },
+    updateData() {
+    },
+    getSortClass: function(key) {
+      const sort = this.listQuery.sort // 'id asc'
+      return sort === `${key} ASC` ? 'ascending' : 'descending'
     }
   }
 }
